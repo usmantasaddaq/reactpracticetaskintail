e84f2f1e174a7ffba5c21444a68cfa6cedfea86b
usman-tasaddaq <usmantasaddaq501@gmail.com>
2022-08-20 01:58:42 +0500

    lecture 8react related work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+ render(){
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="f1 tc">
+      
+        <h1>Hello World{this.props.greeting}</h1>
+        <p>Welcome to React</p>
     </div>
   );
 }
-
+} 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'tachyons';
+// const root = ReactDOM.render(<App greeting={'Hello World this is my 1st app'}/>,document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
